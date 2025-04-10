19a886de07aee5cff9aaefb8449622f2d25c6318
tech.dgarcia <tech.dgarcia@gmail.com>
2025-04-10 10:41:23 -0700

    Update vector_db_setup.py
    Builds the vector stores for each expense category:

--- vector_db_setup.py
+++ vector_db_setup.py
@@ -1,1 +1,12 @@
+from llama_index import VectorStoreIndex, SimpleDirectoryReader
+
+def build_index(folder: str, index_name: str):
+    docs = SimpleDirectoryReader(folder).load_data()
+    index = VectorStoreIndex.from_documents(docs)
+    index.save_to_disk(index_name)
+
+if __name__ == "__main__":
+    build_index("data/travel_docs", "vector_travel")
+    build_index("data/tech_docs", "vector_tech")
+    build_index("data/general_docs", "vector_general")
 
